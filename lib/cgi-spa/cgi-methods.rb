492ee040d9d0954909bba9a5ae45263e56d4634a
Sam Ruby <rubys@intertwingly.net>
2009-12-22 17:30:32 -0500

    Fix typo

--- lib/cgi-spa/cgi-methods.rb
+++ lib/cgi-spa/cgi-methods.rb
@@ -42,10 +42,10 @@ end
 
 # post specific logic (doesn't produce output)
 def $cgi.post
-  yield unless $HTML_POST
+  yield unless $HTTP_POST
 end
 
 # post specific content (produces output)
 def $cgi.post! &block
-  html!(&block) unless $HTML_POST
+  html!(&block) unless $HTTP_POST
 end
--- lib/cgi-spa/version.rb
+++ lib/cgi-spa/version.rb
@@ -2,7 +2,7 @@ module CgiSpa
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 3
-    TINY  = 0
+    TINY  = 1
 
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
