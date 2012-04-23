4302fbeef4a8e3f68a90ff9ef43eb93baa162515
Sam Ruby <rubys@intertwingly.net>
2012-04-23 11:15:29 -0400

    _import! rails helpers

--- lib/wunderbar/rails.rb
+++ lib/wunderbar/rails.rb
@@ -2,13 +2,26 @@ require 'active_support/core_ext'
 
 module Wunderbar
   module Rails
+    class HelperProxy < HtmlMarkup
+      def method_missing(symbol, *args, &block)
+        if @_scope.respond_to? symbol or
+          if @_scope.method(symbol).owner.parents.include?  ActionView::Helpers
+            return _import! @_scope.__send__(symbol, *args, &block)
+          end
+        elsif @_scope.helpers.instance_methods.include? symbol
+          return _import! @_scope.__send__(symbol, *args, &block)
+        end
+        super
+      end
+    end
+
     class HtmlHandler
       cattr_accessor :default_format
       self.default_format = Mime::HTML
 
       def self.call(template)
         pre = %{
-          x = HtmlMarkup.new(self);
+          x = Wunderbar::Rails::HelperProxy.new(self);
           instance_variables.each do |var|
             x.instance_variable_set var, instance_variable_get(var)
           end
