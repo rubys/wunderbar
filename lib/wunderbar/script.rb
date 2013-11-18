ee638a9332b8f4c9cc75835ee6d2ec2f8a9c3f44
Sam Ruby <rubys@intertwingly.net>
2013-11-18 15:07:08 -0500

    pass binding to Ruby2JS

--- lib/wunderbar/script.rb
+++ lib/wunderbar/script.rb
@@ -1,11 +1,20 @@
 require 'wunderbar'
 require 'ruby2js'
 
+# convert script blocks to JavaScript.  If binding_of_caller is available,
+# full access to all variables defined in the callers scope may be made
+# by execute strings (`` or %x()).
+
 module Wunderbar
   class HtmlMarkup
     def _script(*args, &block)
       if block
-        args.unshift Ruby2JS.convert(block)
+        if binding.respond_to? :of_caller
+          # provided by require 'binding_of_caller'
+          args.unshift Ruby2JS.convert(block, binding: binding.of_caller(1))
+        else
+          args.unshift Ruby2JS.convert(block, binding: binding)
+        end
         super *args, &nil
       else
         super
