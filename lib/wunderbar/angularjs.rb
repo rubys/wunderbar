33c87cd0260ff4bd46d383c66c31247310b6d40e
Sam Ruby <rubys@intertwingly.net>
2013-12-19 14:09:36 -0500

    angular templates

--- lib/wunderbar/angularjs.rb
+++ lib/wunderbar/angularjs.rb
@@ -4,3 +4,16 @@ require 'ruby2js/filter/angularrb'
 source = File.expand_path('../vendor/angular.min.js', __FILE__)
 
 Wunderbar::Asset.script :name => 'angular-min.js', :file => source
+
+module Wunderbar
+  class HtmlMarkup
+    def _ng_template(attrs={}, &block)
+      if attrs.empty? and not block
+        proxiable_tag! :ng_template
+      else
+        attrs[:type] ||= 'text/ng-template'
+        tag! :script, attrs, &block
+      end
+    end
+  end
+end
---
+++ test/test_angular.rb
@@ -0,0 +1,17 @@
+require 'test/unit'
+require 'wunderbar/angularjs'
+
+class AngularjsMarkupTest < Test::Unit::TestCase
+  def test_template
+    @x = Wunderbar::HtmlMarkup.new(Struct.new(:params).new({}))
+    @x.html do
+      _ng_template id: 'templateContent.html' do
+        _h1 'Hello World!'
+      end
+    end
+    target = @x._.target!
+    assert_match %r{<script .*id="templateContent.html".*>}, target
+    assert_match %r{<script .*type="text/ng-template".*>}, target
+    assert_match %r{<h1>Hello World!</h1>}, target
+  end
+end
