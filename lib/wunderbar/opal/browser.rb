128793690d212e5fcf8aa3b2a16b44fa100f74de
Sam Ruby <rubys@intertwingly.net>
2015-02-11 11:35:32 -0500

    retire opal integration

--- lib/wunderbar/opal/browser.rb
+++
@@ -1,5 +0,0 @@
-require 'wunderbar/opal'
-require 'opal/browser'
-
-Wunderbar::Asset.script :name => 'opal-browser.js',
-  :contents => Opal::Builder.build('browser')
--- lib/wunderbar/opal/jquery.rb
+++
@@ -1,6 +0,0 @@
-require 'wunderbar/opal'
-require 'wunderbar/jquery'
-require 'opal-jquery'
-
-Wunderbar::Asset.script :name => 'opal-jquery.js',
-  :contents => Opal::Builder.build('opal-jquery')
