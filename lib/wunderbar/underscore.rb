dd7cce86d4109c49ae3741ef4b322f76fce4075c
Sam Ruby <rubys@intertwingly.net>
2016-02-14 13:21:51 -0500

    make underscore eligable to be used by react

--- lib/wunderbar/underscore.rb
+++ lib/wunderbar/underscore.rb
@@ -3,4 +3,4 @@ require 'ruby2js/filter/underscore'
 require 'ruby2js/filter/functions'
 
 source = File.expand_path('../vendor/underscore-min.js', __FILE__)
-Wunderbar::Asset.script :name => 'underscore-min.js', :file => source
+Wunderbar::Asset.script name: 'underscore-min.js', file: source, react: true
