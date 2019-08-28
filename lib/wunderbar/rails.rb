f8d6307787a09bf2940676d287512051e67ff50f
Sam Ruby <rubys@intertwingly.net>
2019-08-28 14:20:50 -0400

    make second parameter optional in order to accommodate Rails 5

--- lib/wunderbar/rails.rb
+++ lib/wunderbar/rails.rb
@@ -7,7 +7,7 @@ module Wunderbar
       cattr_accessor :default_format
       self.default_format = Mime[:html]
 
-      def self.call(template, source)
+      def self.call(template, source=nil)
         %{
           compiled = Proc.new {#{template.source}}
           x = Wunderbar::HtmlMarkup.new(self);
@@ -24,7 +24,7 @@ module Wunderbar
       cattr_accessor :default_format
       self.default_format = Mime[:json]
 
-      def self.call(template, source)
+      def self.call(template, source=nil)
         %{
           compiled = Proc.new {#{template.source}}
           x = Wunderbar::JsonBuilder.new(self);
